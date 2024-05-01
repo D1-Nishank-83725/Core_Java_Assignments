bdec2716899d43dddd31ad3be3b467f669d4db91
D1-Nishank-83725 <nishank.kose77@gmail.com>
2024-05-01 16:32:49 +0530

    Assignment 9 Question 3 Done

---
+++ Assignment_9/Assignment9_Q3/src/Program.java
@@ -0,0 +1,38 @@
+import java.util.StringTokenizer;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		String str = "The class String includes methods for examining individual characters of the sequence, for comparing strings, for searching strings, for extracting substrings, and for creating a copy of a string with all characters translated to uppercase or to lowercase. Case mapping is based on the Unicode Standard version specified by the Character class.";
+		
+//		System.out.println(str.length());
+
+
+		String[] wordsarr = str.split(" ");
+		
+
+		System.out.println("Number of Words in the String: " + wordsarr.length);
+		
+//		int counter = 0;
+//		for (String word : wordsarr) {
+//			counter++;
+//		}
+//		
+//		System.out.println("Number of Words in the String: " + counter);
+		
+		
+		
+		
+		
+//		int noOfWords = 0;
+		
+//		Direct in-built Functions 
+		StringTokenizer strt = new StringTokenizer(str, " ");
+//		System.out.println(strt.countTokens());
+		
+
+	}
+
+}
